d3450cf39c23850d50090d6f732e15a7dece2b35
А. Гербер <Gerber-av@yandex.ru>
2025-04-06 09:58:22 +0700

    Corrected the comments of the first work

--- Assets/Scripts/Counter.cs
+++ Assets/Scripts/Counter.cs
@@ -1,38 +1,67 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Counter : MonoBehaviour
 {
     [SerializeField] private float _startValue = 0.0f;
     [SerializeField] private float _step = 1.0f;
-
-    private float _currentValue;
-
-    public float StartValue => _startValue;
-
-    public bool IsWork { get; private set; } = false;
+    [SerializeField] private float _delay = 0.5f;
 
     public event Action<float> Changed;
 
-    public void MakeStep()
+    private WaitForSeconds _wait;
+    private float _currentValue;
+    private bool _isWork = false;
+
+    public float StartValue => _startValue;
+
+    private void Start()
+    {
+        _currentValue = _startValue;
+        _wait = new WaitForSeconds(_delay);
+    }
+
+    private void Update()
+    {
+        InputReader();
+    }
+
+    private void InputReader()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ManageWork();
+        }
+    }
+
+    private void ManageWork()
+    {
+        if (_isWork == false)
+        {
+            _isWork = true;
+            StartCoroutine(Count());
+        }
+        else
+        {
+            _isWork = false;
+            StopCoroutine(Count());
+        }
+    }
+
+    private void MakeStep()
     {
         _currentValue += _step;
 
         Changed?.Invoke(_currentValue);
     }
 
-    public void Enable()
+    private IEnumerator Count()
     {
-        IsWork = true;
-    }
-
-    public void Disable()
-    {
-        IsWork = false;
-    }
-
-    private void Start()
-    {
-        _currentValue = _startValue;
+        while (_isWork)
+        {
+            yield return _wait;
+            MakeStep();
+        }
     }
 }
--- Assets/Scripts/CounterView.cs
+++ Assets/Scripts/CounterView.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -6,31 +5,11 @@ public class CounterView : MonoBehaviour
 {
     [SerializeField] private TextMeshPro _counterText;
     [SerializeField] private Counter _counter;
-    [SerializeField] private float _delay = 0.5f;
 
-    private WaitForSeconds _wait;
 
     private void Start()
     {
         _counterText.text = _counter.StartValue.ToString("");
-        _wait = new WaitForSeconds(_delay);
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (_counter.IsWork == false)
-            {
-                _counter.Enable();
-                StartCoroutine(Name());
-            }
-            else
-            {
-                _counter.Disable();
-                StopCoroutine(Name());
-            }
-        }
     }
 
     private void OnEnable()
@@ -46,14 +25,5 @@ public class CounterView : MonoBehaviour
     private void OutputCurrentValue(float currentValue)
     {
         _counterText.text = currentValue.ToString("");
-    }
-
-    private IEnumerator Name()
-    {
-        while (_counter.IsWork)
-        {
-            yield return _wait;
-            _counter.MakeStep();
-        }
     }
 }
